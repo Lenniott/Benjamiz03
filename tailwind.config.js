baa9ff2c5acc12574b4572f5d4ddf84772997301
Ben Kanagroo <78645337+Lenniott@users.noreply.github.com>
2023-10-22 06:34:23 +0000

    Update 15 files
    all working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      colors: {
+        // Light colors
+        "primary-light": "#F8FAFC",
+        "secondary-light": "#F8FAFC",
+        "ternary-light": "#F1F5F9",
+
+        // Dark colors
+        "primary-dark": "#1E293B",
+        "secondary-dark": "rgb(30 41 59)",
+        "ternary-dark": "#0F172A",
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "5rem",
+          xl: "6rem",
+          "2xl": "8rem",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
